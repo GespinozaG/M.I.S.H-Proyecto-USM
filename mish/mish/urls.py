ca9b171d2b7073fdb7a0714e2a5898c24065f33b
jhoel <jcanaviri@usm.cl>
2025-10-04 11:50:22 -0300

    Primer commit del prototipo Django

--- mish/mish/urls.py
+++ mish/mish/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path , include
+from prueba import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name='index'),  # La raíz ahora apunta a inicio
+    path('prueba/', include('prueba.urls'))# # Aquí se conecta con tu app
 ]
+
+
